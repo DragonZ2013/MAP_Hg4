6b99a7823c22c79662479edb5a5a2447fcd61ee1
DragonZ <drag.popescu@yahoo.com>
2021-11-03 11:08:02 +0200

    Initial commit

---
+++ src/Main/Repository/CourseRepository.java
@@ -0,0 +1,5 @@
+package Main.Repository;
+
+public class CourseRepository extends InMemoryRepository<Course>{
+
+}
---
+++ src/Main/Repository/InMemoryRepository.java
@@ -0,0 +1,32 @@
+package Main.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class InMemoryRepository<T> implements CrudRepository<T> {
+
+
+    protected List<T> repoList;
+
+    public InMemoryRepository() {
+        this.repoList = new ArrayList<>();
+
+    }
+
+    @Override
+    public T create(T obj) {
+        this.repoList.add(obj);
+        return obj;
+    }
+
+    @Override
+    public List<T> getAll() {
+        return this.repoList;
+    }
+
+    @Override
+    public void delete(T obj) {
+        this.repoList.remove(obj);
+    }
+
+}
