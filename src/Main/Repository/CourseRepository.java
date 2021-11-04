fdfea636b4fcc5724692ef1726992684689dbfac
DragonZ <drag.popescu@yahoo.com>
2021-11-04 12:22:17 +0200

    Course update

--- src/Main/Repository/CourseRepository.java
+++ src/Main/Repository/CourseRepository.java
@@ -1,6 +1,7 @@
 package Main.Repository;
 
 import Main.Model.Course;
+import Main.Model.Student;
 
 public class CourseRepository extends InMemoryRepository<Course>{
 
@@ -10,6 +11,16 @@ public class CourseRepository extends InMemoryRepository<Course>{
 
     @Override
     public Course update(Course obj) {
-        return null;
+        Course courseToUpdate = this.repoList.stream()
+                .filter(course -> course.getCourseId() == obj.getCourseId())
+                .findFirst()
+                .orElseThrow();
+        courseToUpdate.setCredits(obj.getCredits());
+        courseToUpdate.setMaxEnrollment(obj.getMaxEnrollment());
+        courseToUpdate.setName(obj.getName());
+        courseToUpdate.setTeacher(obj.getTeacher());
+        courseToUpdate.setStudentsEnrolled(courseToUpdate.getStudentsEnrolled());
+
+        return courseToUpdate;
     }
 }
