408c8ca796944d38d9a73e2f855315ee578de0c2
DragonZ <drag.popescu@yahoo.com>
2021-11-04 16:00:37 +0200

    JavaDocs for Teacher

--- src/Main/Model/Teacher.java
+++ src/Main/Model/Teacher.java
@@ -7,6 +7,10 @@ public class Teacher extends Person{
     private List<Course> courses;
     private int teacherId;
 
+    /**
+     * Conversion to String for Teacher objects
+     * @return String
+     */
     @Override
     public String toString() {
         return "Teacher{" +
@@ -17,6 +21,10 @@ public class Teacher extends Person{
                 '}';
     }
 
+    /**
+     * Converts the course array to an int array, used to avoid recursive printing
+     * @return List<Integer>
+     */
     public List<Integer> coursesToId(){
         List<Integer> retList = new ArrayList<>();
         for(Course c : this.courses)
@@ -24,24 +32,47 @@ public class Teacher extends Person{
         return retList;
     }
 
+    /**
+     * getter for Teacher.teacherId
+     * @return int
+     */
     public int getTeacherId() {
         return teacherId;
     }
 
+    /**
+     * setter for Teacher.teacherId
+     * @param teacherId
+     */
     public void setTeacherId(int teacherId) {
         this.teacherId = teacherId;
     }
 
+    /**
+     * Constructor for Teacher objects
+     * @param firstName
+     * @param lastName
+     * @param courses
+     * @param teacherId
+     */
     public Teacher(String firstName, String lastName, List<Course> courses,int teacherId) {
         super(firstName, lastName);
         this.courses = courses;
         this.teacherId = teacherId;
     }
 
+    /**
+     * Getter for Teacher.courses
+     * @return List<Courses>
+     */
     public List<Course> getCourses() {
         return courses;
     }
 
+    /**
+     * Setter for Teacher.courses
+     * @param courses
+     */
     public void setCourses(List<Course> courses) {
         this.courses = courses;
     }
