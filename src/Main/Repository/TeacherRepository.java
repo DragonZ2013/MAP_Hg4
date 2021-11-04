1ed36a4ff1781e1ba274bd2473dfce41746f0dcd
DragonZ <drag.popescu@yahoo.com>
2021-11-04 12:20:06 +0200

    Teacher update

--- src/Main/Repository/TeacherRepository.java
+++ src/Main/Repository/TeacherRepository.java
@@ -1,5 +1,6 @@
 package Main.Repository;
 
+import Main.Model.Student;
 import Main.Model.Teacher;
 
 public class TeacherRepository extends InMemoryRepository<Teacher>{
@@ -11,6 +12,14 @@ public class TeacherRepository extends InMemoryRepository<Teacher>{
 
     @Override
     public Teacher update(Teacher obj) {
-        return null;
+        Teacher teacherToUpdate = this.repoList.stream()
+                .filter(teacher -> teacher.getTeacherId() == obj.getTeacherId())
+                .findFirst()
+                .orElseThrow();
+        teacherToUpdate.setCourses(obj.getCourses());
+        teacherToUpdate.setFirstName(obj.getFirstName());
+        teacherToUpdate.setLastName(obj.getLastName());
+
+        return teacherToUpdate;
     }
 }
