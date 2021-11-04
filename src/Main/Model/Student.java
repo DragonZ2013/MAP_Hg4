09d35c57f340302e0beb1fed13c16d86ee242953
DragonZ <drag.popescu@yahoo.com>
2021-11-04 15:58:07 +0200

    JavaDocs for Student

--- src/Main/Model/Student.java
+++ src/Main/Model/Student.java
@@ -8,6 +8,14 @@ public class Student extends Person{
     private int totalCredits;
     private List<Course> enrolledCourses;
 
+    /**
+     * Constructor for Student objects
+     * @param firstName
+     * @param lastName
+     * @param studentId
+     * @param totalCredits
+     * @param enrolledCourses
+     */
     public Student(String firstName, String lastName, int studentId, int totalCredits, List<Course> enrolledCourses) {
         super(firstName, lastName);
         this.studentId = studentId;
@@ -15,6 +23,10 @@ public class Student extends Person{
         this.enrolledCourses = enrolledCourses;
     }
 
+    /**
+     * Converstion to String for Student objects
+     * @return String
+     */
     @Override
     public String toString() {
         return "Student{" +
@@ -26,6 +38,10 @@ public class Student extends Person{
                 '}';
     }
 
+    /**
+     * Converts the course array to an int array, used to avoid recursive printing
+     * @return List<Integer>
+     */
     public List<Integer> coursesToId(){
         List<Integer> retList = new ArrayList<>();
         for(Course c : this.enrolledCourses)
@@ -33,26 +49,50 @@ public class Student extends Person{
         return retList;
     }
 
+    /**
+     * getter for Student.studentId
+     * @return int
+     */
     public int getStudentId() {
         return studentId;
     }
 
+    /**
+     * setter for Student.studentId
+     * @param studentId
+     */
     public void setStudentId(int studentId) {
         this.studentId = studentId;
     }
 
+    /**
+     * getter for Student.totalCredits
+     * @return int
+     */
     public int getTotalCredits() {
         return totalCredits;
     }
 
+    /**
+     * setter for Student.totalCredits
+     * @param totalCredits
+     */
     public void setTotalCredits(int totalCredits) {
         this.totalCredits = totalCredits;
     }
 
+    /**
+     * getter for Student.enrolledCourses
+     * @return List<Course>
+     */
     public List<Course> getEnrolledCourses() {
         return enrolledCourses;
     }
 
+    /**
+     * setter for Student.enrolledCourses
+     * @param enrolledCourses
+     */
     public void setEnrolledCourses(List<Course> enrolledCourses) {
         this.enrolledCourses = enrolledCourses;
     }
