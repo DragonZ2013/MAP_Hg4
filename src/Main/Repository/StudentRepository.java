97d733da00a02ae0055cc7bfbc00477d9127a08c
DragonZ <drag.popescu@yahoo.com>
2021-11-04 16:04:55 +0200

    JavaDocs for StudentRepository

--- src/Main/Repository/StudentRepository.java
+++ src/Main/Repository/StudentRepository.java
@@ -4,10 +4,18 @@ import Main.Model.Student;
 
 public class StudentRepository extends InMemoryRepository<Student>{
 
+    /**
+     * Constructor for StudentRepository objects
+     */
     public StudentRepository() {
         super();
     }
 
+    /**
+     * Updates a Student object from the Memory Repository array given the ID of the parameter object
+     * @param obj
+     * @return Student -modified object
+     */
     @Override
     public Student update(Student obj) {
         Student studentToUpdate = this.repoList.stream()
