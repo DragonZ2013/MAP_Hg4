aa9633c85de50ac35633a79334850becf5c3c218
DragonZ <drag.popescu@yahoo.com>
2021-11-04 13:13:48 +0200

    RegistrationSystem Methods Declaration

--- src/Main/Model/RegistrationSystem.java
+++ src/Main/Model/RegistrationSystem.java
@@ -1,5 +1,24 @@
 package Main.Model;
 
+import java.util.List;
+
 public class RegistrationSystem {
+
+    boolean register (Course course, Student student){
+        return false;
+    }
+
+    List<Course> retrieveCoursesWithFreePlaces(){
+        return null;
+    }
+
+    List<Student> retrieveStudentsEnrolledForACourse(Course course){
+        return null;
+    }
+
+    List<Course> getAllCourses()
+    {
+        return null;
+    }
 
 }
