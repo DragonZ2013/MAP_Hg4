79d97b397769700497e77753af4f2589d87554c3
DragonZ <drag.popescu@yahoo.com>
2021-11-05 09:47:53 +0200

    Tests getAllCourses - WIP!

--- Test/Main/Model/RegistrationSystemTest.java
+++ Test/Main/Model/RegistrationSystemTest.java
@@ -1,6 +1,12 @@
 package Main.Model;
 
+import Main.Repository.CourseRepository;
+import Main.Repository.StudentRepository;
+import Main.Repository.TeacherRepository;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,6 +28,26 @@ class RegistrationSystemTest {
     @Test
     void getAllCourses() {
         RegistrationSystem rs = new RegistrationSystem();
+        CourseRepository cr = new CourseRepository();
+        StudentRepository sr = new StudentRepository();
+        TeacherRepository tr = new TeacherRepository();
+        rs.setCr(cr);
+        rs.setSr(sr);
+        rs.setTr(tr);
+        Teacher t1 = new Teacher("Bob","William",new ArrayList(),1);
+        Course c1 = new Course("name1",t1,5,new ArrayList(),20,1);
+        Student s1 = new Student("Emil","Iulian",1,0,new ArrayList());
+        cr.create(c1);
+        sr.create(s1);
+        tr.create(t1);
+        assertEquals(rs.getAllCourses(),Arrays.asList(c1));
+        Course c2 = new Course("Course1",t1,7,new ArrayList(),20,1);
+        cr.update(c2);
+        assertEquals(rs.getAllCourses(),Arrays.asList(c2));
+        c1.setCourseId(2);
+        assertEquals(rs.getAllCourses(), Arrays.asList(c2,c1));
+
+
 
     }
 }
